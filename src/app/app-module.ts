305724aee4b99ab029250b12fb902fb75c3e7b90
Laura <lagonzalez@birt.eus>
2026-01-21 19:44:03 +0100

    Generado componentes

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { PostList } from './components/post-list/post-list';
+import { PostForm } from './components/post-form/post-form';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    PostList,
+    PostForm
   ],
   imports: [
     BrowserModule,
