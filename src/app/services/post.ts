9f4ff9132af1b2d50d414ef3c6a19c8217ede060
Laura <lagonzalez@birt.eus>
2026-01-21 19:39:05 +0100

    CReado y programado servicio

---
+++ src/app/services/post.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Post } from '../models/post';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class PostService {
+ private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+ constructor(private http:HttpClient) {}
+
+    getPosts(): Observable<Post[]> {
+        return this.http.get<Post[]>(this.apiUrl);
+    }
+
+    getPostById(id:number): Observable<Post> {
+        const url = '${this.apiUrl}/${id}';
+        return this.http.get<Post>(url)
+    }
+
+    createPost(post:Post): Observable<Post> {
+        return this.http.post<Post>(this.apiUrl,post);
+    }
+
+    updatePost(post:Post): Observable<Post> {
+        const url = '${this.apiUrl}/${post.id}';
+        return this.http.put<Post>(url,post)
+    }
+
+    deletePost(id:number): Observable<void> {
+        const url = '${this.apiUrl}/${id}';
+        return this.http.delete<void>(url);
+    }
+}
