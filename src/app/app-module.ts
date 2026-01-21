0ec4d7b26842ee03c7372c0259cb881540260269
Laura <lagonzalez@birt.eus>
2026-01-21 19:51:44 +0100

    Enrutado

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,5 +1,7 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -14,7 +16,9 @@ import { PostForm } from './components/post-form/post-form';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PostList } from './components/post-list/post-list';
+import { PostForm } from './components/post-form/post-form';
+
+const routes: Routes = [
+    { path: 'posts', component: PostList },
+    { path: 'posts/new', component:PostForm },
+    { path: 'posts/edit/:id', component:PostForm },
+    { path: '', redirectTo: '/posts', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
